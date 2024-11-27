39ab30c41e45782eadc3297e5e7d946045a080b7
Shailendu95 <cshailendu@gmail.com>
2024-11-27 20:56:12 +0530

    created the token entity and repository and implemented user repository,role repository and security configuration.

---
+++ book-network/src/main/java/com/group/book/role/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.group.book.role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+    Optional<Role> findByName(String role);
+}
---
+++ book-network/src/main/java/com/group/book/security/JwtFilter.java
@@ -0,0 +1,5 @@
+package com.group.book.security;
+import org.springframework.stereotype.Service;
+@Service
+public class JwtFilter {
+}
