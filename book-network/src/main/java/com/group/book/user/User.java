67d853e2b70b7878e38a8feda2a846d6ff88553b
copiness <aklantabhukta775@gmail.com>
2024-12-08 18:25:44 +0530

    updated the jwt secret-key

--- book-network/src/main/java/com/group/book/user/User.java
+++ book-network/src/main/java/com/group/book/user/User.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.security.Principal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -29,28 +30,20 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Integer id;
-
     private String firstName;
     private String lastName;
-
     private LocalDate dateOfBirth;
 
     @Column(unique = true)
     private String email;
-
     private String password;
-
     private boolean accountLocked;
-
     private boolean enabled;
-
     @ManyToMany(fetch = FetchType.EAGER)
     private List<Role> roles;
-
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
-
     @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime lastModifiedDate;
