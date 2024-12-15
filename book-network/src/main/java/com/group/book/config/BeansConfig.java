85b338974a77c2d660ca0df12bea5178a72c675f
Shailendu95 <cshailendu@gmail.com>
2024-12-15 09:51:24 +0530

    added the application auditor aware and implemented save book,find book by id and find all desplayable books

--- book-network/src/main/java/com/group/book/config/BeansConfig.java
+++ book-network/src/main/java/com/group/book/config/BeansConfig.java
@@ -4,6 +4,7 @@ package com.group.book.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -29,6 +30,12 @@ public class BeansConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
+    }
+
 
 
     @Bean
