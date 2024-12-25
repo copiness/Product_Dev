bf9196477ffb7866d45b77bcad709cd6eb21fe23
priyarajmohnaty01 <119916872+jwjjhehsu@users.noreply.github.com>
2024-12-25 23:51:59 +0530

    ft 2  complete

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -1,11 +1,32 @@
-import { Routes } from '@angular/router';
+// @ts-ignore
+
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
+import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent }, // Default route leading to the AppComponent
+  { path: 'login',
+    component: LoginComponent
+  }, // Default route leading to the AppComponent
 
-  { path: 'register', component: RegisterComponent }
-  // Add more routes here as needed
+  { path: 'register',
+    component: RegisterComponent
+  },
+
+  {
+    path:  'activate-account' ,
+    component: ActivateAccountComponent
+  },
+  {
+    path : 'books',
+    loadChildren:( ) => import('./modules/book/book.module').then(m => m.BookModule),
+  }
 ];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
